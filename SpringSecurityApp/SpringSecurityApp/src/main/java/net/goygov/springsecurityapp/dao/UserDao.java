45f0bda4959637fbf7e01e292e797b8faeee99ee
Said Goygov <56646486+Morgxth@users.noreply.github.com>
2021-06-02 14:22:34 +0300

    Delete SpringSecurityApp/SpringSecurityApp directory

--- SpringSecurityApp/SpringSecurityApp/src/main/java/net/goygov/springsecurityapp/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package net.goygov.springsecurityapp.dao;
-
-import net.goygov.springsecurityapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserDao extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-
-}
--- SpringSecurityApp/SpringSecurityApp/src/main/java/net/goygov/springsecurityapp/service/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package net.goygov.springsecurityapp.service;
-
-public interface SecurityService {
-
-    String findLoggedInUsername();
-
-    void autoLogin(String username, String password);
-
-}
